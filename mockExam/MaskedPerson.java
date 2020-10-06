6d898ed4da3a32cfc26f072357389cd38b311bf4
KwanHW <kwanhaowei@gmail.com>
2020-10-06 09:05:00 +0800

    Adjusted the Person Classes for the last level

--- mockExam/MaskedPerson.java
+++ mockExam/MaskedPerson.java
@@ -1,15 +1,35 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class MaskedPerson extends Person {
     public MaskedPerson(String name) {
         super(name);
     }
 
+    public MaskedPerson(String name, List<Virus> virusList){
+        super(name,virusList);
+    }
+
+    public MaskedPerson(String name,List<Virus> virusList,boolean onSHN,double endOfSHN){
+        super(name,virusList,endOfSHN);
+    }
+
+    @Override
     public List<Virus> transmit(double random) {
-        List<Virus> vList = new ArrayList<>();
-        for(Virus v : this.virusList){
-            v = v.spread(random);
-            vList.add(v);
+        if (random > SimulationParameters.MASK_EFFECTIVENESS) {
+            return super.transmit(random);
+        } else {
+            return List.of();
         }
-        return vList;
+    }
+
+    @Override
+    public Person infectWith(List<Virus> listOfViruses, double random) {
+        if (random > SimulationParameters.MASK_EFFECTIVENESS) {
+            return new MaskedPerson(super.getName(), listOfViruses);
+        } else {
+            return this;
+        }
     }
 
 }
--- mockExam/Person.java
+++ mockExam/Person.java
@@ -4,14 +4,20 @@ import java.util.ArrayList;
 public class Person {
     private final String name;
     private final List<Virus> virusList;
+    private final double endOfSHN;
 
-    public Person(String name, List<Virus> virusList){
+    public Person(String name,List<Virus> virusList,double endOfSHN){
         this.name = name;
         this.virusList = virusList;
+        this.endOfSHN = endOfSHN;
+    }
+
+    public Person(String name, List<Virus> virusList) {
+        this(name, virusList,0);
     }
 
     public Person(String name) {
-        this(name, new ArrayList<Virus>());
+        this(name, new ArrayList<Virus>(),0);
     }
 
     public String getName() {
@@ -20,6 +26,10 @@ public class Person {
 
     public List<Virus> getViruses() {
         return this.virusList;
+    }
+    
+    public double getEndOfSHN() {
+        return this.endOfSHN;
     }
 
     public List<Virus> transmit(double random) {
@@ -35,6 +45,10 @@ public class Person {
         return new Person(name, listOfViruses);
     }
 
+    public Person issuedSHN(double endOfSHN) {
+        return new Person(this.name,this.virusList,endOfSHN);
+    }
+
     public boolean test(String name) {
         for(Virus v : this.virusList) {
             if (v.getName() == name) {
@@ -42,6 +56,11 @@ public class Person {
             }
         }
         return false;
+    }
+
+    // Checks if person is on SHN at specified point in time
+    public boolean onSHN (double currentTime) {
+        return currentTime < endOfSHN;
     }
 
     @Override
